67baa3f225f29f943ceb6a2236309c9a65c25e7a
Luke <lr98lr@aol.com>
2020-05-28 09:51:20 -0400

    glfw push until vulkan supported

--- RARE/RARE/src/main.cpp
+++ RARE/RARE/src/main.cpp
@@ -1,7 +1,10 @@
+#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
 int main() {
 
-	
+	GLFWwindow* window = glfwCreateWindow(640, 480, "POG", NULL, NULL);
+	glfwMakeContextCurrent(window);
+	while (true);
 	return 4;
 }
