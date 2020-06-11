8040becb62343995687e97dd5314523fd46dcf81
Luke <lr98lr@aol.com>
2020-06-11 08:52:39 -0400

    commited model resource and wind?

--- RARE/RARE/src/RareCore.cpp
+++ RARE/RARE/src/RareCore.cpp
@@ -223,7 +223,7 @@ namespace Rare {
 
 				vertex.texCoord = {
 					attrib.texcoords[2 * index.texcoord_index + 0],
-					1.0f- attrib.texcoords[2 * index.texcoord_index + 1]
+					attrib.texcoords[2 * index.texcoord_index + 1]
 				};
 
 				vertex.color = { 1.0f, 1.0f, 1.0f };
