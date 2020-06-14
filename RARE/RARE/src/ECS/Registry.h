18766ecd1a7d4219302cbc1da216fde3514a567a
Luke <lr98lr@aol.com>
2020-06-14 18:30:10 -0400

    creating ecs based renderer prototype

---
+++ RARE/RARE/src/ECS/Registry.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <glm/vec3.hpp>
+namespace Rare {
+	namespace ECS {
+		
+		namespace Handles {
+			typedef unsigned int handle;
+			constexpr handle TransformComponent = 0;
+			constexpr handle RenderableComponent= 1;
+		}
+
+		struct Component {
+			Handles::handle _HANDLE_;
+		};
+		struct TransformComponent : public Component{
+			glm::vec3 position;
+			glm::vec3 rotation;
+			glm::vec3 scale;
+			TransformComponent() { _HANDLE_ = Handles::TransformComponent; }
+
+		};
+	
+
+
+	}
+}
